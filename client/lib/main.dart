c8358522239670df58855c91b7181506c4833658
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-02-03 18:31:59 +0100

    feature: Initial cleanup of client app

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,115 +1,223 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:implicitly_animated_reorderable_list/implicitly_animated_reorderable_list.dart';
+import 'package:implicitly_animated_reorderable_list/transitions.dart';
+import 'package:provider/provider.dart';
+
+import 'package:material_floating_search_bar/material_floating_search_bar.dart';
+
+import 'place.dart';
+import 'search_model.dart';
 
 void main() {
-  runApp(const MyApp());
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.white,
+    ),
+  );
+
+  runApp(
+    MaterialApp(
+      title: 'Material Floating Search Bar Example',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light().copyWith(
+        iconTheme: const IconThemeData(
+          color: Color(0xFF4d4d4d),
+        ),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          elevation: 4,
+        ),
+      ),
+      home: Directionality(
+        textDirection: TextDirection.ltr,
+        child: ChangeNotifierProvider(
+          create: (_) => SearchModel(),
+          child: const Home(),
+        ),
+      ),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomeState createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeState extends State<Home> {
+  final controller = FloatingSearchBarController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  int _index = 0;
+  int get index => _index;
+  set index(int value) {
+    _index = min(value, 2);
+    _index == 2 ? controller.hide() : controller.show();
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      resizeToAvoidBottomInset: false,
+      drawer: Drawer(
+        child: Container(
+          width: 200,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: buildSearchBar(),
     );
+  }
+
+  Widget buildSearchBar() {
+    final actions = [
+      FloatingSearchBarAction(
+        showIfOpened: false,
+        child: CircularButton(
+          icon: const Icon(Icons.place),
+          onPressed: () {},
+        ),
+      ),
+      FloatingSearchBarAction.searchToClear(
+        showIfClosed: false,
+      ),
+    ];
+
+    final isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
+
+    return Consumer<SearchModel>(
+      builder: (context, model, _) => FloatingSearchBar(
+        automaticallyImplyBackButton: false,
+        controller: controller,
+        clearQueryOnClose: true,
+        hint: 'Search...',
+        iconColor: Colors.grey,
+        transitionDuration: const Duration(milliseconds: 800),
+        transitionCurve: Curves.easeInOutCubic,
+        physics: const BouncingScrollPhysics(),
+        axisAlignment: isPortrait ? 0.0 : -1.0,
+        openAxisAlignment: 0.0,
+        actions: actions,
+        progress: model.isLoading,
+        debounceDelay: const Duration(milliseconds: 500),
+        onQueryChanged: model.onQueryChanged,
+        scrollPadding: EdgeInsets.zero,
+        transition: CircularFloatingSearchBarTransition(spacing: 16),
+        builder: (context, _) => buildExpandableBody(model),
+      ),
+    );
+  }
+
+  Widget buildExpandableBody(SearchModel model) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: Material(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+        clipBehavior: Clip.antiAlias,
+        child: ImplicitlyAnimatedList<Place>(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          items: model.suggestions,
+          insertDuration: const Duration(milliseconds: 700),
+          itemBuilder: (context, animation, item, i) {
+            return SizeFadeTransition(
+              animation: animation,
+              child: buildItem(context, item),
+            );
+          },
+          updateItemBuilder: (context, animation, item) {
+            return FadeTransition(
+              opacity: animation,
+              child: buildItem(context, item),
+            );
+          },
+          areItemsTheSame: (a, b) => a == b,
+        ),
+      ),
+    );
+  }
+
+  Widget buildItem(BuildContext context, Place place) {
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+
+    final model = Provider.of<SearchModel>(context, listen: false);
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        InkWell(
+          onTap: () {
+            FloatingSearchBar.of(context)?.close();
+            Future.delayed(
+              const Duration(milliseconds: 500),
+                  () => model.clear(),
+            );
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              children: [
+                SizedBox(
+                  width: 36,
+                  child: AnimatedSwitcher(
+                    duration: const Duration(milliseconds: 500),
+                    child: model.suggestions == history
+                        ? const Icon(Icons.history, key: Key('history'))
+                        : const Icon(Icons.place, key: Key('place')),
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        place.name,
+                        style: textTheme.subtitle1,
+                      ),
+                      const SizedBox(height: 2),
+                      Text(
+                        place.level2Address,
+                        style: textTheme.bodyText2?.copyWith(color: Colors.grey.shade600),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        if (model.suggestions.isNotEmpty && place != model.suggestions.last)
+          const Divider(height: 0),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
