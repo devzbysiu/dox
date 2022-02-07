f0691b969990f19218a2583ac084d28eb984372c
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-02-07 18:20:16 +0100

    feature: Change the way the list is constructed

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,283 +1,389 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:implicitly_animated_reorderable_list/implicitly_animated_reorderable_list.dart';
-import 'package:implicitly_animated_reorderable_list/transitions.dart';
-import 'package:material_floating_search_bar/material_floating_search_bar.dart';
-import 'package:photo_view/photo_view.dart';
-import 'package:provider/provider.dart';
-
-import 'document.dart';
-import 'search_model.dart';
 
 void main() {
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(
-      systemNavigationBarColor: Colors.white,
-    ),
-  );
-
-  runApp(
-    MaterialApp(
-      title: 'Dox Client',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light().copyWith(
-        iconTheme: const IconThemeData(
-          color: Color(0xFF4d4d4d),
-        ),
-        floatingActionButtonTheme: const FloatingActionButtonThemeData(
-          elevation: 4,
-        ),
-      ),
-      home: Directionality(
-        textDirection: TextDirection.ltr,
-        child: ChangeNotifierProvider(
-          create: (_) => SearchModel(),
-          child: const Home(),
-        ),
-      ),
-    ),
-  );
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack);
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Dox Client',
-      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Home(),
+      home: const MyHomePage(title: 'ListView with Search'),
     );
   }
 }
 
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   @override
-  _HomeState createState() => _HomeState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _HomeState extends State<Home> {
-  final controller = FloatingSearchBarController();
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController editingController = TextEditingController();
 
-  int _index = 0;
+  final duplicateItems = List<String>.generate(10000, (i) => "Item $i");
+  var items = <String>[];
 
-  int get index => _index;
+  @override
+  void initState() {
+    items.addAll(duplicateItems);
+    super.initState();
+  }
 
-  set index(int value) {
-    _index = min(value, 2);
-    _index == 2 ? controller.hide() : controller.show();
-    setState(() {});
+  void filterSearchResults(String query) {
+    List<String> dummySearchList = <String>[];
+    dummySearchList.addAll(duplicateItems);
+    if (query.isNotEmpty) {
+      List<String> dummyListData = <String>[];
+      for (var item in dummySearchList) {
+        if (item.contains(query)) {
+          dummyListData.add(item);
+        }
+      }
+      setState(() {
+        items.clear();
+        items.addAll(dummyListData);
+      });
+      return;
+    } else {
+      setState(() {
+        items.clear();
+        items.addAll(duplicateItems);
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      drawer: Drawer(
-        child: Container(
-          width: 200,
-        ),
-      ),
-      body: buildSearchBar(),
-    );
-  }
-
-  Widget buildSearchBar() {
-    final isPortrait =
-        MediaQuery.of(context).orientation == Orientation.portrait;
-
-    return Consumer<SearchModel>(
-      builder: (context, model, _) => FloatingSearchBar(
-        automaticallyImplyBackButton: false,
-        controller: controller,
-        clearQueryOnClose: true,
-        hint: 'Search...',
-        iconColor: Colors.grey,
-        transitionDuration: const Duration(milliseconds: 800),
-        transitionCurve: Curves.easeInOutCubic,
-        physics: const BouncingScrollPhysics(),
-        axisAlignment: isPortrait ? 0.0 : -1.0,
-        openAxisAlignment: 0.0,
-        progress: model.isLoading,
-        debounceDelay: const Duration(milliseconds: 500),
-        onQueryChanged: model.onQueryChanged,
-        scrollPadding: EdgeInsets.zero,
-        transition: CircularFloatingSearchBarTransition(spacing: 16),
-        body: buildBody(model),
-        builder: (context, _) => buildExpandableBody(model),
-      ),
-    );
-  }
-
-  Widget buildBody(SearchModel model) {
-    final children = model.suggestions
-        .map(toImageUrl)
-        .map((url) => OpenableImage(url: url))
-        .toList();
-    return ListView(children: children);
-  }
-
-  String toImageUrl(Document doc) {
-    return "http://10.0.2.2:8000/document/${doc.filename}";
-  }
-
-  Widget buildExpandableBody(SearchModel model) {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 16),
-      child: Material(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(8),
-        clipBehavior: Clip.antiAlias,
-        child: ImplicitlyAnimatedList<Document>(
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          items: model.suggestions,
-          insertDuration: const Duration(milliseconds: 700),
-          itemBuilder: (context, animation, item, i) {
-            return SizeFadeTransition(
-              animation: animation,
-              child: buildItem(context, item),
-            );
-          },
-          updateItemBuilder: (context, animation, item) {
-            return FadeTransition(
-              opacity: animation,
-              child: buildItem(context, item),
-            );
-          },
-          areItemsTheSame: (a, b) => a == b,
-        ),
-      ),
-    );
-  }
-
-  Widget buildItem(BuildContext context, Document doc) {
-    final theme = Theme.of(context);
-    final textTheme = theme.textTheme;
-
-    final model = Provider.of<SearchModel>(context, listen: false);
-
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        InkWell(
-          onTap: () {
-            FloatingSearchBar.of(context)?.close();
-            Future.delayed(
-              const Duration(milliseconds: 500),
-              () => model.clear(),
-            );
-          },
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Row(
-              children: [
-                SizedBox(
-                  width: 36,
-                  child: AnimatedSwitcher(
-                    duration: const Duration(milliseconds: 500),
-                    child: model.suggestions == history
-                        ? const Icon(Icons.history, key: Key('history'))
-                        : const Icon(Icons.document_scanner,
-                            key: Key('document')),
-                  ),
-                ),
-                const SizedBox(width: 16),
-                Expanded(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        doc.filename,
-                        style: textTheme.subtitle1,
-                      ),
-                      const SizedBox(height: 2),
-                      Text(
-                        doc.filename,
-                        style: textTheme.bodyText2
-                            ?.copyWith(color: Colors.grey.shade600),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              onChanged: (value) {
+                filterSearchResults(value);
+              },
+              controller: editingController,
+              decoration: const InputDecoration(
+                  labelText: "Search",
+                  hintText: "Search",
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(25.0)))),
             ),
           ),
-        ),
-        if (model.suggestions.isNotEmpty && doc != model.suggestions.last)
-          const Divider(height: 0),
-      ],
-    );
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-}
-
-class OpenableImage extends StatelessWidget {
-  final String url;
-
-  const OpenableImage({required this.url});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: GestureDetector(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => HeroPhotoViewRouteWrapper(
-                imageProvider: NetworkImage(url),
-              ),
+          Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(items[index]),
+                );
+              },
             ),
-          );
-        },
-        child: Hero(
-          tag: "someTag",
-          child: Image.network(
-            url,
-            width: 350.0,
-            loadingBuilder: (_, child, chunk) =>
-                chunk != null ? const Text("loading") : child,
           ),
-        ),
+        ],
       ),
     );
   }
 }
 
-class HeroPhotoViewRouteWrapper extends StatelessWidget {
-  final ImageProvider imageProvider;
-  final BoxDecoration? backgroundDecoration;
-
-  const HeroPhotoViewRouteWrapper({
-    required this.imageProvider,
-    this.backgroundDecoration,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      constraints: BoxConstraints.expand(
-        height: MediaQuery.of(context).size.height,
-      ),
-      child: PhotoView(
-        imageProvider: imageProvider,
-        backgroundDecoration: backgroundDecoration,
-      ),
-    );
-  }
-}
+// import 'dart:math';
+//
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:implicitly_animated_reorderable_list/implicitly_animated_reorderable_list.dart';
+// import 'package:implicitly_animated_reorderable_list/transitions.dart';
+// import 'package:material_floating_search_bar/material_floating_search_bar.dart';
+// import 'package:photo_view/photo_view.dart';
+// import 'package:provider/provider.dart';
+//
+// import 'document.dart';
+// import 'search_model.dart';
+//
+// void main() {
+//   SystemChrome.setSystemUIOverlayStyle(
+//     const SystemUiOverlayStyle(
+//       systemNavigationBarColor: Colors.white,
+//     ),
+//   );
+//
+//   runApp(
+//     MaterialApp(
+//       title: 'Dox Client',
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData.light().copyWith(
+//         iconTheme: const IconThemeData(
+//           color: Color(0xFF4d4d4d),
+//         ),
+//         floatingActionButtonTheme: const FloatingActionButtonThemeData(
+//           elevation: 4,
+//         ),
+//       ),
+//       home: Directionality(
+//         textDirection: TextDirection.ltr,
+//         child: ChangeNotifierProvider(
+//           create: (_) => SearchModel(),
+//           child: const Home(),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Dox Client',
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: const Home(),
+//     );
+//   }
+// }
+//
+// class Home extends StatefulWidget {
+//   const Home({Key? key}) : super(key: key);
+//
+//   @override
+//   _HomeState createState() => _HomeState();
+// }
+//
+// class _HomeState extends State<Home> {
+//   final controller = FloatingSearchBarController();
+//
+//   int _index = 0;
+//
+//   int get index => _index;
+//
+//   set index(int value) {
+//     _index = min(value, 2);
+//     _index == 2 ? controller.hide() : controller.show();
+//     setState(() {});
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       resizeToAvoidBottomInset: false,
+//       drawer: Drawer(
+//         child: Container(
+//           width: 200,
+//         ),
+//       ),
+//       body: buildSearchBar(),
+//     );
+//   }
+//
+//   Widget buildSearchBar() {
+//     final isPortrait =
+//         MediaQuery.of(context).orientation == Orientation.portrait;
+//
+//     return Consumer<SearchModel>(
+//       builder: (context, model, _) => FloatingSearchBar(
+//         automaticallyImplyBackButton: false,
+//         controller: controller,
+//         clearQueryOnClose: true,
+//         hint: 'Search...',
+//         iconColor: Colors.grey,
+//         transitionDuration: const Duration(milliseconds: 800),
+//         transitionCurve: Curves.easeInOutCubic,
+//         physics: const BouncingScrollPhysics(),
+//         axisAlignment: isPortrait ? 0.0 : -1.0,
+//         openAxisAlignment: 0.0,
+//         progress: model.isLoading,
+//         debounceDelay: const Duration(milliseconds: 500),
+//         onQueryChanged: model.onQueryChanged,
+//         scrollPadding: EdgeInsets.zero,
+//         transition: CircularFloatingSearchBarTransition(spacing: 16),
+//         body: buildBody(model),
+//         builder: (context, _) => buildExpandableBody(model),
+//       ),
+//     );
+//   }
+//
+//   Widget buildBody(SearchModel model) {
+//     final children = model.suggestions
+//         .map(toImageUrl)
+//         .map((url) => OpenableImage(url: url))
+//         .toList();
+//     return ListView(children: children);
+//   }
+//
+//   String toImageUrl(Document doc) {
+//     return "http://10.0.2.2:8000/document/${doc.filename}";
+//   }
+//
+//   Widget buildExpandableBody(SearchModel model) {
+//     return Container(
+//       padding: const EdgeInsets.symmetric(vertical: 16),
+//       child: Material(
+//         color: Colors.white,
+//         borderRadius: BorderRadius.circular(8),
+//         clipBehavior: Clip.antiAlias,
+//         child: ImplicitlyAnimatedList<Document>(
+//           shrinkWrap: true,
+//           physics: const NeverScrollableScrollPhysics(),
+//           items: model.suggestions,
+//           insertDuration: const Duration(milliseconds: 700),
+//           itemBuilder: (context, animation, item, i) {
+//             return SizeFadeTransition(
+//               animation: animation,
+//               child: buildItem(context, item),
+//             );
+//           },
+//           updateItemBuilder: (context, animation, item) {
+//             return FadeTransition(
+//               opacity: animation,
+//               child: buildItem(context, item),
+//             );
+//           },
+//           areItemsTheSame: (a, b) => a == b,
+//         ),
+//       ),
+//     );
+//   }
+//
+//   Widget buildItem(BuildContext context, Document doc) {
+//     final theme = Theme.of(context);
+//     final textTheme = theme.textTheme;
+//
+//     final model = Provider.of<SearchModel>(context, listen: false);
+//
+//     return Column(
+//       mainAxisSize: MainAxisSize.min,
+//       children: [
+//         InkWell(
+//           onTap: () {
+//             FloatingSearchBar.of(context)?.close();
+//             Future.delayed(
+//               const Duration(milliseconds: 500),
+//               () => model.clear(),
+//             );
+//           },
+//           child: Padding(
+//             padding: const EdgeInsets.all(16),
+//             child: Row(
+//               children: [
+//                 SizedBox(
+//                   width: 36,
+//                   child: AnimatedSwitcher(
+//                     duration: const Duration(milliseconds: 500),
+//                     child: model.suggestions == history
+//                         ? const Icon(Icons.history, key: Key('history'))
+//                         : const Icon(Icons.document_scanner,
+//                             key: Key('document')),
+//                   ),
+//                 ),
+//                 const SizedBox(width: 16),
+//                 Expanded(
+//                   child: Column(
+//                     mainAxisSize: MainAxisSize.min,
+//                     crossAxisAlignment: CrossAxisAlignment.start,
+//                     children: [
+//                       Text(
+//                         doc.filename,
+//                         style: textTheme.subtitle1,
+//                       ),
+//                       const SizedBox(height: 2),
+//                       Text(
+//                         doc.filename,
+//                         style: textTheme.bodyText2
+//                             ?.copyWith(color: Colors.grey.shade600),
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//         if (model.suggestions.isNotEmpty && doc != model.suggestions.last)
+//           const Divider(height: 0),
+//       ],
+//     );
+//   }
+//
+//   @override
+//   void dispose() {
+//     controller.dispose();
+//     super.dispose();
+//   }
+// }
+//
+// class OpenableImage extends StatelessWidget {
+//   final String url;
+//
+//   const OpenableImage({required this.url});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: GestureDetector(
+//         onTap: () {
+//           Navigator.push(
+//             context,
+//             MaterialPageRoute(
+//               builder: (context) => HeroPhotoViewRouteWrapper(
+//                 imageProvider: NetworkImage(url),
+//               ),
+//             ),
+//           );
+//         },
+//         child: Hero(
+//           tag: "someTag",
+//           child: Image.network(
+//             url,
+//             width: 350.0,
+//             loadingBuilder: (_, child, chunk) =>
+//                 chunk != null ? const Text("loading") : child,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+//
+// class HeroPhotoViewRouteWrapper extends StatelessWidget {
+//   final ImageProvider imageProvider;
+//   final BoxDecoration? backgroundDecoration;
+//
+//   const HeroPhotoViewRouteWrapper({
+//     required this.imageProvider,
+//     this.backgroundDecoration,
+//   });
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       constraints: BoxConstraints.expand(
+//         height: MediaQuery.of(context).size.height,
+//       ),
+//       child: PhotoView(
+//         imageProvider: imageProvider,
+//         backgroundDecoration: backgroundDecoration,
+//       ),
+//     );
+//   }
+// }
