e4b78b3f200a1444e9656ec480526393a800e2be
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-02-24 11:52:46 +0100

    refactor: Improve error messages

--- core/src/result.rs
+++ core/src/result.rs
@@ -6,55 +6,55 @@ use thiserror::Error;
 // TODO: cleanup error messages and names
 #[derive(Debug, Error)]
 pub enum DoxErr {
-    #[error("failed to create or write file: '{0}'")]
+    #[error("Failed to create or write file: '{0}'")]
     Io(#[from] std::io::Error),
 
-    #[error("failed to decode from base64: '{0}'")]
+    #[error("Failed to decode from base64: '{0}'")]
     Decode(#[from] base64::DecodeError),
 
-    #[error("indexer failure: '{0}'")]
+    #[error("Indexer failure: '{0}'")]
     Indexing(#[from] tantivy::TantivyError),
 
-    #[error("error during query parsing: '{0}'")]
+    #[error("Error during query parsing: '{0}'")]
     Parsing(#[from] tantivy::query::QueryParserError),
 
-    #[error("error from fs watcher: '{0}'")]
+    #[error("Error from fs watcher: '{0}'")]
     FsWatcher(#[from] notify::Error),
 
-    #[error("error when sending path through channel: '{0}'")]
+    #[error("Error when sending path through channel: '{0}'")]
     Send(#[from] std::sync::mpsc::SendError<PathBuf>),
 
-    #[error("error when receiving list of paths through channel: '{0}'")]
+    #[error("Error when receiving list of paths through channel: '{0}'")]
     Recv(#[from] std::sync::mpsc::RecvError),
 
-    #[error("error when deserializing from toml: '{0}'")]
+    #[error("Error when deserializing from TOML: '{0}'")]
     TomlDe(#[from] toml::de::Error),
 
-    #[error("error when initializing LepTess: '{0}'")]
+    #[error("Error when initializing LepTess: '{0}'")]
     OcrExtract(#[from] leptess::tesseract::TessInitError),
 
-    #[error("error when setting image: '{0}'")]
+    #[error("Error when setting image: '{0}'")]
     Image(#[from] leptess::leptonica::PixError),
 
-    #[error("error when converting to utf8: '{0}'")]
+    #[error("Error when converting to utf8: '{0}'")]
     Utf8(#[from] std::str::Utf8Error),
 
-    #[error("error when extracting text from pdf: '{0}'")]
+    #[error("Error when extracting text from PDF: '{0}'")]
     PdfExtract(#[from] pdf_extract::OutputError),
 
-    #[error("error while displaying prompt: '{0}'")]
+    #[error("Error while displaying prompt: '{0}'")]
     Prompt(#[from] inquire::error::InquireError),
 
-    #[error("error while serializing configuration: '{0}'")]
+    #[error("Error while serializing configuration: '{0}'")]
     TomlSe(#[from] toml::ser::Error),
 
-    #[error("error while creating pdf thumnail surface: '{0}'")]
+    #[error("Error while creating PDF thumnail surface: '{0}'")]
     ThumbnailSurface(#[from] cairo::Error),
 
-    #[error("error while thumnail writing thumbnail to file: '{0}'")]
+    #[error("Error writing thumbnail to file: '{0}'")]
     CarioIo(#[from] cairo::IoError),
 
-    #[error("error while creating poppler document for pdf thumbnail: '{0}'")]
+    #[error("Error while creating poppler document for PDF thumbnail: '{0}'")]
     Poppler(#[from] cairo::glib::error::Error),
 }
 
