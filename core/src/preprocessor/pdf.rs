128242aecb849f783072e02774001aaefab0029d
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-02-26 17:18:00 +0100

    refactor: Cleanup code

--- core/src/preprocessor/pdf.rs
+++ core/src/preprocessor/pdf.rs
@@ -1,13 +1,13 @@
 use log::debug;
 
+use crate::helpers::PathRefExt;
 use crate::preprocessor::FilePreprocessor;
 use crate::result::Result;
 use crate::thumbnail;
 
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 pub struct Pdf {
-    #[allow(unused)] // TODO: Remove that
     thumbnails_dir: PathBuf,
 }
 
@@ -15,17 +15,17 @@ impl Pdf {
     pub fn new(thumbnails_dir: PathBuf) -> Self {
         Self { thumbnails_dir }
     }
+
+    fn thumbnail_path<P: AsRef<Path>>(&self, pdf_path: P) -> PathBuf {
+        self.thumbnails_dir.join(pdf_path.filename())
+    }
 }
 
 impl FilePreprocessor for Pdf {
     fn preprocess(&self, paths: &[PathBuf]) -> Result<()> {
         debug!("generating thumbnails for paths: {:?}", paths);
         for pdf_path in paths {
-            thumbnail::generate(
-                pdf_path,
-                // TODO: take care of this unwrap
-                &self.thumbnails_dir.join(pdf_path.file_name().unwrap()),
-            )?;
+            thumbnail::generate(pdf_path, &self.thumbnail_path(pdf_path))?;
         }
         Ok(())
     }
--- core/src/thumbnail.rs
+++ core/src/thumbnail.rs
@@ -1,4 +1,4 @@
-use crate::helpers::PathRefExt;
+use crate::helpers::{PathExt, PathRefExt};
 use crate::result::Result;
 
 use cairo::{Context, Format, ImageSurface};
@@ -14,15 +14,13 @@ pub fn generate<P: AsRef<Path>>(pdf_path: P, out_path: P) -> Result<()> {
     debug!("generating thumbnail for {}", pdf_path.as_ref().display());
     let page = first_page(&pdf_path)?;
     let surface = paint_background_and_scale(page)?;
-    let filename = pdf_path.filename();
-    debug!("saving PDF thumbnail '{}' ...", filename);
-    let mut f: File = File::create(filename)?;
-    // TODO: thumbnail generation and saving to file should be separated
+    let mut f: File = File::create(pdf_path.filename())?;
     surface.write_to_png(&mut f)?;
     Ok(())
 }
 
 fn first_page<P: AsRef<Path>>(pdf_path: P) -> Result<PopplerPage> {
+    debug!("getting first page of PDF '{}'", pdf_path.as_ref().string());
     let doc: PopplerDocument = PopplerDocument::new_from_file(pdf_path, "")?;
     Ok(doc
         .get_page(FIRST)
@@ -30,6 +28,7 @@ fn first_page<P: AsRef<Path>>(pdf_path: P) -> Result<PopplerPage> {
 }
 
 fn paint_background_and_scale(page: PopplerPage) -> Result<ImageSurface> {
+    debug!("painting while backgroud and scaling");
     let (width, height) = page.get_size();
     #[allow(clippy::cast_possible_truncation)] // TODO: make sure that's necessary
     let surface = ImageSurface::create(Format::Rgb24, width as i32, height as i32)?;
@@ -39,7 +38,6 @@ fn paint_background_and_scale(page: PopplerPage) -> Result<ImageSurface> {
     ctxt.set_source_rgb(1.0, 1.0, 1.0);
     ctxt.scale(1.0, 1.0);
     ctxt.paint()?;
-    // Draw the contents of the PDF onto the page.
     page.render(&ctxt);
     Ok(surface)
 }
