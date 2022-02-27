b06d9eef8c7ddc525b52bf1ab03af06b9a82ba21
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-02-27 10:50:22 +0100

    fix: Fix clippy warnings

--- core/src/thumbnail.rs
+++ core/src/thumbnail.rs
@@ -13,7 +13,7 @@ const FIRST: usize = 0;
 pub fn generate<P: AsRef<Path>>(pdf_path: P, out_path: P) -> Result<()> {
     debug!("generating thumbnail for '{}'", pdf_path.as_ref().display());
     let page = first_page(&pdf_path)?;
-    let surface = paint_background_and_scale(page)?;
+    let surface = paint_background_and_scale(&page)?;
     debug!("writing thumbnail to: '{}'", out_path.as_ref().display());
     let mut f: File = File::create(out_path)?;
     surface.write_to_png(&mut f)?;
@@ -28,7 +28,7 @@ fn first_page<P: AsRef<Path>>(pdf_path: P) -> Result<PopplerPage> {
         .unwrap_or_else(|| panic!("failed to get page")))
 }
 
-fn paint_background_and_scale(page: PopplerPage) -> Result<ImageSurface> {
+fn paint_background_and_scale(page: &PopplerPage) -> Result<ImageSurface> {
     debug!("painting while backgroud and scaling");
     let (width, height) = page.get_size();
     #[allow(clippy::cast_possible_truncation)] // TODO: make sure that's necessary
