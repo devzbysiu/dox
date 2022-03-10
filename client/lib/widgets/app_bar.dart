ca26a6a23cde90934e2ca9843eff7265cea6dda5
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-03-10 19:49:57 +0100

    feature: Add initial version of connection icon

--- client/lib/widgets/app_bar.dart
+++ client/lib/widgets/app_bar.dart
@@ -9,7 +9,16 @@ class ScrollableAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SliverAppBar(
-      title: Text('Dox', style: TextStyle(color: onPrimary(context))),
+      title: Container(
+        width: 15,
+        height: 15,
+        decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [Colors.green[300]!, Colors.yellow[400]!])),
+      ),
       expandedHeight: 220.0,
       flexibleSpace: FlexibleSpaceBar(
         background: Image.asset('assets/app-bar.webp', fit: BoxFit.cover),
