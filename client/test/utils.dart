d96a4dd052877167b845fe3e9abe3fe262c2cb15
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-03-18 15:35:01 +0100

    refactor: Move testing utils to separate module

---
+++ client/test/utils.dart
@@ -0,0 +1,67 @@
+import 'package:dox/models/connection_state.dart';
+import 'package:dox/models/docs_state.dart';
+import 'package:dox/services/connection_service.dart';
+import 'package:dox/services/docs_service.dart';
+import 'package:dox/utilities/config.dart';
+import 'package:dox/utilities/events_stream.dart';
+import 'package:dox/utilities/urls.dart';
+import 'package:dox/widgets/status_dot.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+
+MultiProvider wrapper({
+  required widget,
+  Config? cfg,
+  Urls? urls,
+  Events? ev,
+  DocsService? docs,
+  DocsState? docsSt,
+  ConnService? conn,
+  ConnState? connSt,
+}) {
+  final config = cfg ?? ConfigMock();
+  final urlsProvider = urls ?? Urls(config: config);
+  final events = ev ?? Events(urlsProvider: urlsProvider);
+  final docsService = docs ?? DocsService(urls: urlsProvider, ev: events);
+  final connService = conn ?? ConnService(ev: events);
+  DocsState docsState(_) => docsSt ?? DocsState(docsService: docsService);
+  ConnState connState(_) => connSt ?? ConnStateImpl(connService: connService);
+
+  return MultiProvider(
+    providers: [
+      ChangeNotifierProvider<DocsState>(create: docsState),
+      ChangeNotifierProvider<ConnState>(create: connState),
+    ],
+    child: widget,
+  );
+}
+
+class ConfigMock implements Config {
+  @override
+  String get baseUrl => 'http://192.168.16.247:8000';
+
+  @override
+  String get websocketUrl => 'ws://192.168.16.247:8001';
+}
+
+class ConnStateMock extends ChangeNotifier implements ConnState {
+  bool _isConnected = false;
+
+  @override
+  bool get isConnected => _isConnected;
+
+  set isConnected(val) {
+    _isConnected = val;
+    notifyListeners();
+  }
+}
+
+extension StatusDotExt on StatusDot {
+  List<Color> color(WidgetTester tester) {
+    final Container container = tester.firstWidget(find.byType(Container));
+    final boxDecoration = container.decoration as BoxDecoration;
+    final gradient = boxDecoration.gradient as LinearGradient;
+    return gradient.colors;
+  }
+}
--- client/test/widgets/status_dot_test.dart
+++ client/test/widgets/status_dot_test.dart
@@ -1,37 +1,28 @@
-import 'package:dox/models/connection_state.dart';
-import 'package:dox/models/docs_state.dart';
-import 'package:dox/services/connection_service.dart';
-import 'package:dox/services/docs_service.dart';
-import 'package:dox/utilities/config.dart';
-import 'package:dox/utilities/events_stream.dart';
-import 'package:dox/utilities/urls.dart';
 import 'package:dox/widgets/status_dot.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:get_it/get_it.dart';
-import 'package:provider/provider.dart';
 
-final getIt = GetIt.instance;
+import '../utils.dart';
 
 void main() {
-  testWidgets("StatusDot initially displays gray dot", (tester) async {
+  testWidgets('StatusDot initially displays gray dot', (tester) async {
     // given
-    const statusDot = StatusDot();
+    const statusDot = StatusDot(key: Key('StatusDot'));
 
     // when
-    await tester.pumpWidget(_wrapper(widget: statusDot));
+    await tester.pumpWidget(wrapper(widget: statusDot));
 
     // then
     expect(statusDot.color(tester), equals([Colors.blueGrey, Colors.blueGrey]));
   });
 
-  testWidgets("StatusDot changes color when connected", (tester) async {
+  testWidgets('StatusDot changes color when connected', (tester) async {
     // given
-    final connState = _ConnStateMock();
+    final connState = ConnStateMock();
     const statusDot = StatusDot();
 
     // when
-    await tester.pumpWidget(_wrapper(widget: statusDot, connSt: connState));
+    await tester.pumpWidget(wrapper(widget: statusDot, connSt: connState));
     expect(statusDot.color(tester), equals([Colors.blueGrey, Colors.blueGrey]));
 
     connState.isConnected = true;
@@ -43,60 +34,4 @@ void main() {
       equals([Colors.green[300]!, Colors.yellow[400]!]),
     );
   });
-}
-
-MultiProvider _wrapper({
-  required widget,
-  Config? cfg,
-  Urls? urls,
-  Events? ev,
-  DocsService? docs,
-  DocsState? docsSt,
-  ConnService? conn,
-  ConnState? connSt,
-}) {
-  final config = cfg ?? _ConfigMock();
-  final urlsProvider = urls ?? Urls(config: config);
-  final events = ev ?? Events(urlsProvider: urlsProvider);
-  final docsService = docs ?? DocsService(urls: urlsProvider, ev: events);
-  final connService = conn ?? ConnService(ev: events);
-  DocsState docsState(_) => docsSt ?? DocsState(docsService: docsService);
-  ConnState connState(_) => connSt ?? ConnStateImpl(connService: connService);
-
-  return MultiProvider(
-    providers: [
-      ChangeNotifierProvider<DocsState>(create: docsState),
-      ChangeNotifierProvider<ConnState>(create: connState),
-    ],
-    child: widget,
-  );
-}
-
-class _ConfigMock implements Config {
-  @override
-  String get baseUrl => 'http://192.168.16.247:8000';
-
-  @override
-  String get websocketUrl => 'ws://192.168.16.247:8001';
-}
-
-class _ConnStateMock extends ChangeNotifier implements ConnState {
-  bool _isConnected = false;
-
-  @override
-  bool get isConnected => _isConnected;
-
-  set isConnected(val) {
-    _isConnected = val;
-    notifyListeners();
-  }
-}
-
-extension StatusDotExt on StatusDot {
-  List<Color> color(WidgetTester tester) {
-    final Container container = tester.firstWidget(find.byType(Container));
-    final boxDecoration = container.decoration as BoxDecoration;
-    final gradient = boxDecoration.gradient as LinearGradient;
-    return gradient.colors;
-  }
 }
