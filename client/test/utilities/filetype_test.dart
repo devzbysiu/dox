7f50c0738f30d6413b2ee2abb73f576614eef28f
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-03-18 15:56:29 +0100

    refactor: Cleanup filetype test

--- client/test/utilities/filetype_test.dart
+++ client/test/utilities/filetype_test.dart
@@ -2,14 +2,27 @@ import 'package:dox/utilities/filetype.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('Uri to file returns correct filetype', () {
-    // given
-    final uri = Uri.parse("http://some-address.com/file.jpg");
+  group('Uri to supported filetypes returns correct filetype', () {
+    final extToFiletype = {
+      'jpg': Filetype.image,
+      'jpeg': Filetype.image,
+      'webp': Filetype.image,
+      'png': Filetype.image,
+      'pdf': Filetype.pdf,
+    };
+    extToFiletype.forEach((input, expected) {
+      test('Uri to $input file returns correct filetype', () {
+        // given
+        final uri = Uri.parse("http://some-address.com/file.$input");
 
-    // when
-    final filetype = uri.filetype();
+        // when
+        final filetype = uri.filetype();
 
-    // then
-    expect(filetype, Filetype.image);
+        // then
+        expect(filetype, expected);
+      });
+    });
   });
+
+  
 }
