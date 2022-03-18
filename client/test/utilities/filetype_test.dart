518f8d9a844a7105776cb8202cbbb9732b96fd8d
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-03-18 15:58:55 +0100

    test: Add tests for getters

--- client/test/utilities/filetype_test.dart
+++ client/test/utilities/filetype_test.dart
@@ -24,5 +24,65 @@ void main() {
     });
   });
 
+  group('Uri to unsupported filetypes returns Filetype.other', () {
+    final extToFiletype = {
+      'abc': Filetype.other,
+      '': Filetype.other,
+      'docx': Filetype.other,
+      'doc': Filetype.other,
+      'tex': Filetype.other,
+    };
+    extToFiletype.forEach((input, expected) {
+      test('Uri to $input file returns Filetype.other', () {
+        // given
+        final uri = Uri.parse("http://some-address.com/file.$input");
 
+        // when
+        final filetype = uri.filetype();
+
+        // then
+        expect(filetype, expected);
+      });
+    });
+  });
+
+  group('isImage return correct results', () {
+    final filetypeToResult = {
+      Filetype.image: true,
+      Filetype.pdf: false,
+      Filetype.other: false,
+    };
+    filetypeToResult.forEach((input, expected) {
+      test('isImage for $input returns $expected', () {
+        // given
+        final filetype = input;
+
+        // when
+        final isImage = filetype.isImage;
+
+        // then
+        expect(isImage, expected);
+      });
+    });
+  });
+
+  group('isPdf return correct results', () {
+    final filetypeToResult = {
+      Filetype.image: false,
+      Filetype.pdf: true,
+      Filetype.other: false,
+    };
+    filetypeToResult.forEach((input, expected) {
+      test('isPdf for $input returns $expected', () {
+        // given
+        final filetype = input;
+
+        // when
+        final isPdf = filetype.isPdf;
+
+        // then
+        expect(isPdf, expected);
+      });
+    });
+  });
 }
