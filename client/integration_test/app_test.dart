7038209ffd15392625c68333dcfb31fd9dfdda73
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-03-28 21:54:10 +0200

    refactor: Remove unused import

--- client/integration_test/app_test.dart
+++ client/integration_test/app_test.dart
@@ -1,9 +1,7 @@
+import 'package:dox/main.dart' as app;
 import 'package:dox/widgets/document/openable_document.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-
-import 'package:dox/main.dart' as app;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
