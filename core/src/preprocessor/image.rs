83f71af7a9625d6c2f37cd30f3828215a670426f
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-04-06 16:45:01 +0200

    test: Cover image preprocessor with wrong file

--- core/src/preprocessor/image.rs
+++ core/src/preprocessor/image.rs
@@ -53,4 +53,26 @@ mod test {
 
         Ok(())
     }
+
+    #[test]
+    fn test_preprocess_with_wrong_files() -> Result<()> {
+        // given
+        let tmp_dir = tempdir()?;
+        let preprocessor = Image::new(&tmp_dir);
+        let paths = &[PathBuf::from("res/doc1.pdf")];
+        let is_empty = tmp_dir.path().read_dir()?.next().is_none();
+        assert!(is_empty);
+
+        // TODO: currently, this just copies the file to the thumbnails_dir without
+        // checking if this is the correct file type. Potentially this should be checked
+        // and error should be thrown (and this should be consistent with Pdf preprocessor)
+        // when
+        preprocessor.preprocess(paths)?;
+        let file = tmp_dir.path().read_dir()?.next().unwrap()?.filename();
+
+        // then
+        assert_eq!(file, "doc1.pdf");
+
+        Ok(())
+    }
 }
