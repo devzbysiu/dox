c9714151d669dcb61261d7f6f3e9e3a89b591ee8
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-04-06 17:49:16 +0200

    test: Add test for preprocessor factory

--- core/src/preprocessor/mod.rs
+++ core/src/preprocessor/mod.rs
@@ -28,3 +28,50 @@ impl PreprocessorFactory {
 }
 
 pub type Preprocessor = Box<dyn FilePreprocessor>;
+
+#[cfg(test)]
+mod test {
+    use tempfile::tempdir;
+
+    use crate::helpers::DirEntryExt;
+
+    use super::*;
+    use std::time::Duration;
+
+    #[test]
+    fn test_processor_factory_with_corrent_file() -> Result<()> {
+        // given
+        let test_cases = vec![
+            (Ext::Png, "res/doc1.png", "doc1.png"),
+            (Ext::Jpg, "res/doc3.jpg", "doc3.jpg"),
+            (Ext::Webp, "res/doc4.webp", "doc4.webp"),
+            (Ext::Pdf, "res/doc1.pdf", "doc1.png"),
+        ];
+
+        for test_case in test_cases {
+            let ext = test_case.0;
+
+            let thumbnails_dir = tempdir()?;
+            let config = Config {
+                watched_dir: PathBuf::from("not-important"),
+                index_dir: PathBuf::from("not-important"),
+                thumbnails_dir: thumbnails_dir.path().to_path_buf(),
+                cooldown_time: Duration::from_secs(1),
+            };
+            // when
+            let extractor = PreprocessorFactory::from_ext(&ext, &config);
+            extractor.preprocess(&[PathBuf::from(test_case.1)])?;
+
+            // then
+            let filename = config
+                .thumbnails_dir
+                .read_dir()?
+                .next()
+                .unwrap()?
+                .filename();
+            assert_eq!(filename, test_case.2);
+        }
+
+        Ok(())
+    }
+}
