32a79e3733d86548c24bb7f74955d869c18cb374
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-04-19 14:56:46 +0200

    refactor: Add initial version of ConfigLoader

--- core/src/data_providers/config_loader.rs
+++ core/src/data_providers/config_loader.rs
@@ -1,17 +1,29 @@
 use crate::cfg::Config;
-use crate::result::Result;
+use crate::result::{DoxErr, Result};
 use crate::use_cases::config::ConfigLoader;
 
+use std::fs::create_dir_all;
+use std::fs::{read_to_string, File};
+use std::io::prelude::*;
 use std::path::PathBuf;
+use tracing::instrument;
 
 pub struct FsConfigLoader;
 
 impl ConfigLoader for FsConfigLoader {
+    #[instrument(skip(self))]
     fn load(&self, path: PathBuf) -> Result<Config> {
-        unimplemented!();
+        Ok(toml::from_str(&read_to_string(path)?)?)
     }
 
-    fn store(&self, path: PathBuf, config: &Config) -> Result<()> {
-        unimplemented!();
+    #[instrument(skip(self))]
+    fn store(&self, path: PathBuf, cfg: &Config) -> Result<()> {
+        let config_dir = path.parent().ok_or_else(|| {
+            DoxErr::InvalidConfigPath("Can't use '/' as a configuration path".into())
+        })?;
+        create_dir_all(config_dir)?;
+        let mut file = File::create(path)?;
+        file.write_all(toml::to_string(cfg)?.as_bytes())?;
+        Ok(())
     }
 }
