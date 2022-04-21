7f39533dfc7b7e40cb392e744106786d19035937
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-04-21 23:11:15 +0200

    docs: Add initial docs for text extractor

--- core/src/use_cases/extractor.rs
+++ core/src/use_cases/extractor.rs
@@ -1,3 +1,4 @@
+//! Represents abstractions for extracting text.
 use crate::entities::document::DocDetails;
 use crate::entities::extension::Ext;
 use crate::entities::location::Location;
@@ -5,14 +6,18 @@ use crate::result::Result;
 
 use std::path::PathBuf;
 
+/// Extracts text.
 #[allow(clippy::module_name_repetitions)]
 pub trait TextExtractor {
     fn extract_text(&self, path: &[PathBuf]) -> Vec<DocDetails>;
+    /// Given the [`Location`], extracts text from all documents contained in it.
     fn extract_text_from_location(&self, location: &Location) -> Result<Vec<DocDetails>>;
 }
 
+/// Creates extractor.
 #[allow(clippy::module_name_repetitions)]
 pub trait ExtractorFactory: Sync + Send {
+    /// Creates different extractors based on the provided extension.
     fn from_ext(&self, ext: &Ext) -> Extractor;
 }
 
