0d308a49fd53ab5aeddb3763a3f4ee5e2e121451
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-04-21 22:53:18 +0200

    docs: Add initial docs of the location mod

--- core/src/entities/location.rs
+++ core/src/entities/location.rs
@@ -1,16 +1,27 @@
+//! Abstraction representing the location on a medium, containing set of documents.
+//!
+//! This is medium agnostic, so the particular way the documents are read is part of the
+//! implementation.
 use crate::entities::extension::Ext;
 use crate::helpers::PathRefExt;
 
 use std::path::PathBuf;
 
+/// Represents abstraction of the location on some medium.
 #[derive(Debug)]
 #[allow(unused)]
 pub enum Location {
+    /// Represents local disk as a medium.
     FileSystem(Vec<PathBuf>),
 }
 
 #[allow(unused)]
 impl Location {
+    /// Provides extension of the file which location points to.
+    ///
+    /// This implementation assumes that all documents appearing in the system via one particular
+    /// event, have the same extension. It's achieved by getting first path of the vector of paths
+    /// and reading extension of this path.
     pub fn extension(&self) -> Ext {
         let Location::FileSystem(paths) = self;
         paths
