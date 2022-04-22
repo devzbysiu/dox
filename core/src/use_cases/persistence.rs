8e1a638745be9bc0e9ebe4ea657625a325051dc7
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-04-22 13:50:47 +0200

    docs: Add initial docs for persistence mod

--- core/src/use_cases/persistence.rs
+++ core/src/use_cases/persistence.rs
@@ -1,7 +1,13 @@
+//! Abstraction used to store received documents.
 use std::path::PathBuf;
 
 use crate::result::Result;
 
+/// Abstracts the process of saving document.
+///
+/// The actual implementation can be as simple as saving file on disk, saving file in
+/// the cloud, or keeping it in memory. The implemention details are left for the implementer.
 pub trait Persistence: Sync + Send {
+    /// Saves buffer passed as second argument under path specified as first argument.
     fn save(&self, uri: PathBuf, buf: &[u8]) -> Result<()>;
 }
