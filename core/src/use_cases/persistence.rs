108f8046e4ab160126512abe3c7f2a4f61f36f19
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-04-22 13:47:10 +0200

    refactor: Fix formatting

--- core/src/use_cases/persistence.rs
+++ core/src/use_cases/persistence.rs
@@ -3,4 +3,5 @@ use std::path::PathBuf;
 use crate::result::Result;
 
 pub trait Persistence: Sync + Send {
-    fn save(&self, uri: PathBuf, buf: &[u8]) -> Result<()>; }
+    fn save(&self, uri: PathBuf, buf: &[u8]) -> Result<()>;
+}
