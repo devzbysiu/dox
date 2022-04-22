28fd4807403c7e90dd0b8b9b92e5f7cd266dd6d3
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-04-22 14:19:13 +0200

    docs: Add initial docs for thumbnail mod

--- core/src/use_cases/thumbnail.rs
+++ core/src/use_cases/thumbnail.rs
@@ -1,9 +1,13 @@
+//! Abstraction for everything related to thumbnail.
 use crate::result::Result;
 
 use std::fmt::Debug;
 use std::path::Path;
 
+/// Abstracts thumbnail generation. This trait is used to generate thumbnail for PDF documents so
+/// they can be consumed the same way as images by client application.
 #[allow(clippy::module_name_repetitions)]
 pub trait ThumbnailGenerator: Debug {
+    /// Generates thumbnail.
     fn generate(&self, pdf_path: &Path, out_path: &Path) -> Result<()>;
 }
