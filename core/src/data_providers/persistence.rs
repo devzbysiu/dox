8333a17fd670764115fc52d219034c5efd16c8e3
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-04-25 16:42:37 +0200

    refactor: Implement FsPersistence body

--- core/src/data_providers/persistence.rs
+++ core/src/data_providers/persistence.rs
@@ -1,12 +1,16 @@
 use crate::result::Result;
 use crate::use_cases::persistence::Persistence;
 
+use std::fs::File;
+use std::io::Write;
 use std::path::PathBuf;
 
 pub struct FsPersistence;
 
 impl Persistence for FsPersistence {
     fn save(&self, uri: PathBuf, buf: &[u8]) -> Result<()> {
-        unimplemented!()
+        let mut file = File::create(uri)?;
+        file.write_all(buf)?;
+        Ok(())
     }
 }
