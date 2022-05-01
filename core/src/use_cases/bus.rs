ed8206f05f7b00b5d1ab77cb26c6506be5e8b186
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-05-01 12:03:35 +0200

    refactor: Add initial traits for generic bus

--- core/src/use_cases/bus.rs
+++ core/src/use_cases/bus.rs
@@ -3,8 +3,28 @@
 //! The events represent new files of particular document, appearing in the system, which are going
 //! to be indexed by dox' core.
 use crate::entities::location::Location;
+use crate::result::Result;
 
 use std::fmt::Debug;
+
+pub trait Bus {
+    fn subscriber(&self) -> Box<dyn Subscriber>;
+    fn publisher(&self) -> Box<dyn Publisher>;
+    fn publish(&self, event: &Event) -> Result<()>;
+}
+
+pub trait Subscriber {
+    fn recv(&self) -> Result<Event>;
+}
+
+pub enum Event {
+    Internal(InternalEvent),
+    External(ExternalEvent),
+}
+
+pub trait Publisher {
+    fn publish(&self, event: &Event) -> Result<()>;
+}
 
 /// Represents external events happening in the system.
 #[derive(Debug, Clone)]
