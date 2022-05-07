8cc59d0d7df64e01ce19a55542d07c019e93f040
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-05-07 20:41:38 +0200

    refactor: Change log level to debug

--- core/src/configuration/telemetry.rs
+++ core/src/configuration/telemetry.rs
@@ -7,7 +7,7 @@ use tracing_subscriber::{layer::SubscriberExt, EnvFilter};
 static TRACING: Lazy<()> = Lazy::new(setup_global_subscriber);
 
 fn setup_global_subscriber() {
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("debug"));
     let formatting_layer = BunyanFormattingLayer::new("dox".into(), std::io::stdout);
     let subscriber = Registry::default()
         .with(env_filter)
