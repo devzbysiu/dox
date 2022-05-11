1e418b6e9dc19c2ec12f88971308be301eba6f7d
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-05-11 10:44:03 +0200

    refactor: Add initial notifications_stream

---
+++ client/lib/utilities/notifications_stream.dart
@@ -0,0 +1,40 @@
+import 'package:dox/utilities/log.dart';
+import 'package:dox/utilities/service_locator.dart';
+import 'package:dox/utilities/urls.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:web_socket_channel/io.dart';
+
+abstract class NotificationsStream implements ChangeNotifier {
+  Stream get stream;
+}
+
+class NotificationsStreamImpl extends ChangeNotifier
+    with Log
+    implements NotificationsStream {
+  NotificationsStreamImpl({
+    Urls? urlsProvider,
+  }) {
+    final urls = urlsProvider ?? getIt<Urls>();
+    _notificationsUri = urls.notifications();
+    log.fine('initializing EventsStream with URL: "$_notificationsUri"');
+    _stream = _connect();
+  }
+
+  Stream _connect() {
+    return IOWebSocketChannel.connect(_notificationsUri)
+        .stream
+        .asBroadcastStream();
+  }
+
+  void reconnect() {
+    _stream = _connect();
+    notifyListeners();
+  }
+
+  late Stream _stream;
+
+  late final Uri _notificationsUri;
+
+  @override
+  Stream get stream => _stream;
+}
