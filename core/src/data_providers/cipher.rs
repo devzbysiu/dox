be9366497166a55b53f3c106f76854310d46abd6
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-09-13 17:01:39 +0200

    refactor: Move cipher utils to cipher mod

--- core/src/data_providers/cipher.rs
+++ core/src/data_providers/cipher.rs
@@ -1,6 +1,9 @@
-use crate::helpers::cipher::{self, key, nonce};
 use crate::result::Result;
 use crate::use_cases::cipher::{CipherRead, CipherReadStrategy, CipherWrite, CipherWriteStrategy};
+
+use chacha20poly1305::aead::{Aead, OsRng};
+use chacha20poly1305::{AeadCore, Key, KeyInit, XChaCha20Poly1305, XNonce};
+use once_cell::sync::OnceCell;
 
 pub struct Chacha20Poly1305Cipher;
 
@@ -17,7 +20,7 @@ pub struct Chacha20Poly1305Read;
 
 impl CipherReadStrategy for Chacha20Poly1305Read {
     fn decrypt(&self, src_buf: &[u8]) -> Result<Vec<u8>> {
-        cipher::decrypt(src_buf, key(), nonce())
+        decrypt(src_buf, key(), nonce())
     }
 }
 
@@ -25,6 +28,28 @@ pub struct Chacha20Poly1305Write;
 
 impl CipherWriteStrategy for Chacha20Poly1305Write {
     fn encrypt(&self, src_buf: &[u8]) -> Result<Vec<u8>> {
-        cipher::encrypt(src_buf, key(), nonce())
+        encrypt(src_buf, key(), nonce())
     }
+}
+
+// TODO: what about server restarts?
+fn key() -> &'static Key {
+    static KEY_INSTANCE: OnceCell<Key> = OnceCell::new();
+    KEY_INSTANCE.get_or_init(|| XChaCha20Poly1305::generate_key(&mut OsRng))
+}
+
+// TODO: what about server restarts?
+fn nonce() -> &'static XNonce {
+    static NONCE_INSTANCE: OnceCell<XNonce> = OnceCell::new();
+    NONCE_INSTANCE.get_or_init(|| XChaCha20Poly1305::generate_nonce(&mut OsRng))
+}
+
+fn encrypt(src_buf: &[u8], key: &Key, nonce: &XNonce) -> Result<Vec<u8>> {
+    let cipher = XChaCha20Poly1305::new(key);
+    Ok(cipher.encrypt(nonce, src_buf)?)
+}
+
+fn decrypt(src_buf: &[u8], key: &Key, nonce: &XNonce) -> Result<Vec<u8>> {
+    let cipher = XChaCha20Poly1305::new(key);
+    Ok(cipher.decrypt(nonce, src_buf)?)
 }
