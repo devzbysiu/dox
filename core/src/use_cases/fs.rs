6aa3c289ac96ec25d763854fe7bdf76f3ad7ea90
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-12-04 13:35:03 +0100

    docs: Cleanup fs docs

--- core/src/use_cases/fs.rs
+++ core/src/use_cases/fs.rs
@@ -1,4 +1,4 @@
-//! Abstraction used to save received documents.
+//! Abstraction used to work with documents.
 use crate::entities::location::SafePathBuf;
 use crate::result::FsErr;
 
@@ -7,20 +7,19 @@ use std::sync::Arc;
 
 pub type Fs = Arc<dyn Filesystem>;
 
-// TODO: Update docs
-/// Abstracts the process of saving document.
+/// Abstracts the process of manipulating a file resource.
 ///
 /// The actual implementation can be as simple as saving file on disk, saving file in
 /// the cloud, or keeping it in memory. The implemention details are left for the implementer.
 pub trait Filesystem: Sync + Send {
-    /// Saves buffer passed as second argument under path specified as first argument.
+    /// Saves buffer `buf` under path specified under `uri`.
     fn save(&self, uri: PathBuf, buf: &[u8]) -> Result<(), FsErr>;
 
-    /// Loads the file under first argument.
+    /// Loads the file pointed by `uri`.
     fn load(&self, uri: PathBuf) -> Result<Vec<u8>, FsErr>;
 
-    /// Removes file specified by the `path` argument.
-    fn rm_file(&self, path: &SafePathBuf) -> Result<(), FsErr>;
+    /// Removes file specified by the `uri` argument.
+    fn rm_file(&self, uri: &SafePathBuf) -> Result<(), FsErr>;
 
     /// Moves file from `from` path to `to` path.
     fn mv_file(&self, from: &SafePathBuf, to: &Path) -> Result<(), FsErr>;
