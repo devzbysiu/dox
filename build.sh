61515a75fa9ccf6162d033a9a22012e79a6bf7ef
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2023-01-05 15:58:42 +0100

    chore: Improve build script

--- build.sh
+++ build.sh
@@ -1,23 +1,44 @@
 #!/usr/bin/env bash
 
+# This script builds core and/or client when file changes are found.
+# It reads the changes from git, then checks if paths start with
+# `client/` or `core/` respectively. If changed files are not visible
+# in git or do not start with `core/` or `client/` then nothing happens.
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
 # ============================ [ CONFIGURATION ] ============================ 
 
-DEBUG=0
+DEBUG=0 # enable debug logs
 
 CHANGES_IN_CLIENT=1 # no changes
 CHANGES_IN_CORE=1 # no changes
+
+# colors
+
+BOLD="\e[1";
+ENDCOLOR="\e[0m";
+
+# font color selection
+CYAN="36";
+
+# bg color
+BG_DEFAULT="49";
 
 # ============================ [ DECLARATION ] ============================ 
 
 function debug() {
   if [[ "${DEBUG}" -eq 0 ]]; then
-    echo "[DEBUG] $1"
+    local -r message=$1
+    echo -e "${BOLD};${BG_DEFAULT};${CYAN}m [DEBUG] ${ENDCOLOR}${message}"
   fi
 }
 
 function read_changes() {
-  local -r changed_files=$(git status --porcelain | cut -d' ' -f2)
-  debug "changed_files = ${changed_files}"
+  local -r changed_files=$(git status --porcelain | sed 's/^ //g' | tr -s ' ' | cut -d' ' -f2)
+  debug "changed_files = [ ${changed_files} ]"
   while IFS= read -r line; do
     if [[ ${line} = client/* ]]; then
       debug "line ${line} starts with client/*"
@@ -37,7 +58,10 @@ function core_changed() {
 
 function core_build() {
   debug "Building core..."
-  cd ./core && cargo make clip && cargo make clip && cd ../
+  pushd ./core
+  cargo make clip 
+  cargo make test
+  popd
 }
 
 function client_changed() {
@@ -47,7 +71,10 @@ function client_changed() {
 
 function client_build() {
   debug "Building client..."
-  cd ./client && dart format . && flutter test && cd ../
+  pushd ./client
+  dart format .
+  flutter test
+  popd
 }
 
 function main() {
